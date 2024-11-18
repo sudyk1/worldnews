dcca73f186ccf4f48055ca558d0975b64bcef090
adams <108087334+sudyk1@users.noreply.github.com>
2024-11-18 23:03:52 +0100

    Fixed schema.sql

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -10,6 +10,11 @@ CREATE TABLE `category` (
                             UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
+INSERT INTO `category` (name, description) VALUES ('Entertainment', 'This category is your space to share the latest news on movies, music, TV, celebrity updates, fashion, and pop culture. Post articles, reviews, and updates on popular events, celebrity news, new releases, and trends in the arts and media industry to keep everyone in the loop on what’s happening in entertainment around the world.');
+INSERT INTO `category` (name, description) VALUES ('Politics', 'Here, you can share news and insights on political events, government policies, elections, and international relations. Post updates, analysis, and reports on political developments, leaders decisions, party activities, and key geopolitical issues shaping both local and global landscapes.');
+INSERT INTO `category` (name, description) VALUES ('Business', 'This category keeps you informed on the latest in business, including market trends, economic policies, corporate developments, and financial news. Share articles on startup growth, investment insights, industry reports, and major shifts impacting businesses and consumers worldwide.');
+INSERT INTO `category` (name, description) VALUES ('Sports', 'This category is all about keeping up with the action in the world of sports. Share the latest news on matches, tournaments, player transfers, team updates, and major sporting events. Whether it’s football, basketball, tennis, or any other sport, this is the place to keep everyone informed and engaged with the latest highlights and stories.');
+
 CREATE TABLE `user` (
                         `id` int NOT NULL AUTO_INCREMENT,
                         `username` varchar(50) COLLATE utf8mb4_polish_ci NOT NULL,
